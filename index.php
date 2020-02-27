78b711c8a2f2b8e0ec99633c7d543834b6125b4c
omarcortes4040 <omar@4040apps.com>
2020-02-27 12:40:41 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Monitoreo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container-main">
+        <div class="content-principal">
+            <div class="row">
+                <div class="col-6 principal-l">
+                    <div class="instant-txt"><img src="img/arrow_down.png" alt=""><p>99.1 <span>kW</span></p></div>
+                    <div>INSTANTÁNEA</div>
+                    <div>Lorem ipsum dolor sit amet</div>
+                </div>
+                <div class="col-6 principal-r">
+                    <div>PANEL DE MONITOREO | img</div>
+                    <div>img</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <div>124 kW</div>
+                    <div>INSTALADA</div>
+                    <div>Lorem ipsum dolor sit amet</div>
+                </div>
+                <div class="col-3">
+                    <div>23.2 kW</div>
+                    <div>DIARIA</div>
+                    <div>Lorem ipsum dolor sit amet</div>
+                </div>
+                <div class="col-3">
+                    <div>32.1 kW</div>
+                    <div>MENSUAL</div>
+                    <div>Lorem ipsum dolor sit amet</div>
+                </div>
+                <div class="col-3">
+                    <div>3,340 kW</div>
+                    <div>POTENCIA ACUMULADA</div>
+                    <div>Lorem ipsum dolor sit amet</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div>img</div>
+                    <div>
+                        <div>01.1|UV</div>
+                        <div>RADIACIÓN SOLAR</div>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div>img</div>
+                    <div>
+                        <div>23ºC</div>
+                        <div>TEMPERARTURA</div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div>img</div>
+                    <div>
+                        <div>5 km/h</div>
+                        <div>VELOCIDAD DE VIENTO</div>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div>img</div>
+                    <div>
+                        <div>40%</div>
+                        <div>HUMEDAD</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
