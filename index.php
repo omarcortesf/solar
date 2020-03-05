356c7186fdbfd16b9467e5132a651037cc949b6a
omarcortes4040 <omar@4040apps.com>
2020-03-05 14:06:04 -0600

    API weather added

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <?php 
-
-
     $conn = mysqli_connect("localhost","root","", "ElectricalSystem");
     $sql = "SELECT Instantanea, Diaria, Mensual, Acumulada  FROM Fotovoltaico ORDER BY id DESC LIMIT 1";
     $result = mysqli_query($conn, $sql);
     $resutCheck = mysqli_fetch_assoc($result);
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Monitoreo</title>
+    <title>ElectricalSystem</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -57,38 +52,97 @@
             </div>
             <div class="row wearow">
                 <div class="col-4 wea-order">
-                    <div><img src="img/sol.png" alt=""></div>
+                    <div><img src="img/Sol.png" alt=""></div>
                     <div>
-                        <div class="wea-txt">01.1 | UV</div>
+                        <div id="sol_uv" class="wea-txt"></div>
                         <div class="wea-descrip">RADIACIÓN SOLAR</div>
                     </div>
                 </div>
                 <div class="col-2 wea-order">
                     <div><img src="img/temperatura.png" alt=""></div>
                     <div>
-                        <div class="wea-txt">23ºC</div>
+                        <div id="temp" class="wea-txt">23ºC</div>
                         <div class="wea-descrip">TEMPERARTURA</div>
                     </div>
                 </div>
                 <div class="col-4 wea-order">
                     <div><img src="img/aire.png" alt=""></div>
                     <div>
-                        <div class="wea-txt">5 km/h</div>
+                        <div id="wind" class="wea-txt">5 km/h</div>
                         <div class="wea-descrip">VELOCIDAD DE VIENTO</div>
                     </div>
                 </div>
                 <div class="col-2 wea-order wea-order-got">
                     <div><img src="img/gota.png" alt=""></div>
                     <div>
-                        <div class="wea-txt">40%</div>
+                        <div id="hum" class="wea-txt">40%</div>
                         <div class="wea-descrip">HUMEDAD</div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>    
+// CHANGE 
+
+
+function API_weather() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url:
+                'http://api.openweathermap.org/data/2.5/weather?lat=20.673924&lon=-103.365449&appid=727b921beb636692dd0606184ecbc711',
+            type: 'GET',
+            success: function(result, status, xhr) {
+                var temperatura = Math.trunc(result.main.temp - 273);
+                var viento = result.wind.speed;
+                var humedad = result.main.humidity;
+                resolve({
+                    temperatura: temperatura,
+                    viento: viento,
+                    humedad: humedad,
+                });
+            },
+            error: function(err) {
+                reject(err);
+            },
+        });
+    });
+}
+
+function API_uv() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url:
+                'http://api.openweathermap.org/data/2.5/uvi?appid=727b921beb636692dd0606184ecbc711&lat=20.673924&lon=-103.365449',
+            type: 'GET',
+            success: function(result2, status, xhr) {
+                var sol_uv = result2.value;
+                resolve({ sol_uv: sol_uv });
+            },
+            error: function(err) {
+                reject(err);
+            },
+        });
+    });
+}
+window.onload = function() {
+    var prom1 = API_weather();
+    var prom2 = API_uv();
+    Promise.all([prom1, prom2])
+        .then(result => {
+            $('#sol_uv').html(result[1].sol_uv+ '| UV');
+            $('#temp').html(result[0].temperatura + 'ºC');
+            $('#wind').html(result[0].viento + 'km/h');
+            $('#hum').html(result[0].humedad + '%');
+        })
+        .catch(error => {
+            console.error(error);
+        });
+};
+
+</script>
 </body>
 </html>
