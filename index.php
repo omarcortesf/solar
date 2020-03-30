360b9562225bfc83fbb3d633e10b56f7c08e92ac
omarcortes4040 <omar@4040apps.com>
2020-03-30 10:12:47 -0600

    style video

--- index.php
+++ index.php
@@ -104,8 +104,8 @@ $resutCheck = mysqli_fetch_assoc($result);
         </button>
 
         <!-- Modal -->
-        <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-        <div class="modal-xl" role="document">
+        <div class="modal" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
             <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
